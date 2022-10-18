19f94cb733082285d5e27c58b3306a08a1124196
Eduardo <eduardo.hbds@gmail.com>
2022-10-18 17:46:07 -0300

    Created ControladorActivity

---
+++ ControladorActivity.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class ControladorActivity {
+  
+  private List<Activity> activityLista;
+  
+  ControladorActivity(){
+    activityLista=new ArrayList<Activity>();
+  }
+
+  /**
+   * @return the List<Activity>
+   */
+  public List<Activity> getActivity() {
+    return activityLista;
+  }
+
+  public void addActivity(Activity activity) {
+    this.activityLista.add(activity);
+  }
+
+  public void removeActivity(Activity activity) {
+    activityLista.remove(activity);
+  }
+
+  public List<Activity> listActivity() {
+    return activityLista;
+  }
+
+  public void updateActivity(Activity activity) {
+    try {
+      activityLista.set(activityLista.indexOf(activity), activity);
+    } catch (Exception e) {
+      
+    }
+  }
+}
